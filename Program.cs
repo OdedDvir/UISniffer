9d93c2ff2b37f1a70c2d25891fb5dbfd0d3df22b
Oded Dvir Nachum <odeddvirn@gmail.com>
2022-01-26 00:21:56 +0200

    Added option to choose an app by window title or process id.

--- Program.cs
+++ Program.cs
@@ -33,40 +33,56 @@ void Initialize(AutomationType selectedAutomationType)
 {
     _automation = selectedAutomationType == AutomationType.UIA2 ? (AutomationBase)new UIA2Automation() : new UIA3Automation();
     _rootElement = _automation.GetDesktop();
+
     var desktopViewModel = new ElementViewModel(_rootElement);
-    desktopViewModel.LoadChildren(false);
+    desktopViewModel.LoadChildren(true);
     Elements.Add(desktopViewModel);
 }
 
 Initialize(AutomationType.UIA3);
 
 
-if (Environment.GetCommandLineArgs().Length < 3)
+if (Environment.GetCommandLineArgs().Length < 4)
 {
     Console.WriteLine("Syntax:");
-    Console.WriteLine("   InspectUI <Application_Window_Title> <Output_File_Path>");
-    Console.WriteLine("Example:");
-    Console.WriteLine("   InspectUI \"Word\" output.json");
+    Console.WriteLine("   InspectUI -title <Application_Window_Title> <Output_File_Path>");
+    Console.WriteLine("   InspectUI -pid <Process_Id> <Output_File_Path>");
+    Console.WriteLine("Examples:");
+    Console.WriteLine("   InspectUI -title \"Word\" output.json");
+    Console.WriteLine("   InspectUI -pid 20020 output.json");
     return;
 }
 
 // Get command line arguments
-string windowTitle = Environment.GetCommandLineArgs()[1];
-string outFileName = Environment.GetCommandLineArgs()[2];
+string switchType = Environment.GetCommandLineArgs()[1];
+string appIdentifier = Environment.GetCommandLineArgs()[2];
+string outFileName = Environment.GetCommandLineArgs()[3];
 
-
-Console.WriteLine($"Searching for window title {windowTitle}...");
+Console.WriteLine($"Searching for {appIdentifier}...");
 ElementViewModel appRoot;
 try
 {
-    appRoot = Elements[0].Children.Where(e => e.Name.Contains(windowTitle)).First();
+    switch (switchType.ToLower())
+    {
+        case "-title":
+            //Search by window title
+            appRoot = Elements[0].Children.Where(e => e.Name.Contains(appIdentifier)).First();
+            break;
+        case "-pid":
+            //Search by process Id
+            appRoot = Elements[0].Children.Where(e => e.AutomationElement.Properties.ProcessId.ToString() == appIdentifier).First();
+            break;
+        default:
+            return;
+    }
 }
 catch (Exception)
 {
     Console.WriteLine("Could not find app.");
     return;
 }
-Console.WriteLine($"App found! Writing JSON to file {outFileName}...");
+Console.WriteLine("App found!");
+Console.WriteLine($"Writing JSON to file {outFileName}...");
 try
 {
     string jsonUI = JsonConvert.SerializeObject(ScanElements(appRoot));
